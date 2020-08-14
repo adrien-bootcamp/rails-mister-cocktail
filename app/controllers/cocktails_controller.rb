aa609a4ea3eda5d2e60d2e7131b3e997b7109cdb
Adrien Fort <adrien.fort01@gmail.com>
2020-08-14 17:02:48 +0200

    style done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -19,8 +19,13 @@ class CocktailsController < ApplicationController
     else
       render :new
     end
+  end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
 
+    redirect_to cocktails_path
   end
 
   private
